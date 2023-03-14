0ed2cdd80b7328b003c2b02295c779bb6e761e1d
YSergo <102493873+YSergo@users.noreply.github.com>
2023-03-15 02:46:30 +0400

    added header (almost finished layout)

--- src/App.js
+++ src/App.js
@@ -3,11 +3,31 @@ import './App.scss';
 
 function App() {
   return (
-    <div>
-      {/* <header className="App-header">
-        <img width={60} height={60} src={logo} className="App-logo" alt="logo" />
-      </header> */}
+    <div className="app">
+      <header>
+        <div className="headerLeft">
+          <button>Codify.Software</button>
+        </div>
+        <div className="headerCenter">
+          <button>О нас</button>
+          <button>Услуги</button>
+          <button>Портфолио</button>
+        </div>
+        <div className="headerRight">
+          <div>
+            <button>
+              <p>RU</p>
+              <svg width="15" height="8" viewBox="0 0 15 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M14 1L7.5 7L1 1" stroke="black" stroke-linecap="square" />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </header>
     </div>
+    // {/* <header className="App-header">
+    //   <img width={60} height={60} src={logo} className="App-logo" alt="logo" />
+    // </header> */}
   );
 }
 
