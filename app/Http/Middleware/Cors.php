5503147d46ed37e843edc9407c93fab19c27c729
Bogdan <toporovbo@gmail.com>
2023-03-31 20:50:56 +0300

    cors

---
+++ app/Http/Middleware/Cors.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class Cors
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $response = $next($request);
+
+        if($request->header('Origin')) {
+            $response->header('Access-Control-Allow-Origin', $request->header('Origin'));
+        } else {
+            $response->header('Access-Control-Allow-Origin', '*');
+        }
+        $response->header('Access-Control-Allow-Credentials', 'true');
+
+        return $response;
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,13 @@
 <?php
 
 use App\Http\Controllers\Api\ProjectsController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ServiceController;
 use App\Http\Controllers\Api\ApplicationController;
 
-Route::get('/get-services', [ServiceController::class, 'getServices']);
-Route::get('/get-projects', [ProjectsController::class, 'getProjects']);
+Route::middleware(['cors'])->group(function () {
+    Route::get('/get-services', [ServiceController::class, 'getServices']);
+    Route::get('/get-projects', [ProjectsController::class, 'getProjects']);
 
-Route::post('/send-application', [ApplicationController::class, 'handleApplication']);
-
+    Route::post('/send-application', [ApplicationController::class, 'handleApplication']);
+});
