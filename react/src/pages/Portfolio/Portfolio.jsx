3c04d81e69d4b51a45ce84a4e6f5c4de59b7b45e
YSergo <102493873+YSergo@users.noreply.github.com>
2023-04-14 08:08:51 +0400

    +desktop skeletons

--- react/src/pages/Portfolio/Portfolio.jsx
+++ react/src/pages/Portfolio/Portfolio.jsx
@@ -4,38 +4,65 @@ import styles from "./Portfolio.module.scss";
 import PortfolioCard from "../../components/PortfolioCard/PortfolioCard";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import ContentLoader from "react-content-loader";
 
 function Portfolio() {
   const [projects, setProjects] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function fetchData() {
       try {
+        setLoading(true);
         const response = await axios.get(
           "https://codify.software/api/get-projects"
         );
         setProjects(response.data);
+        setLoading(false);
       } catch (error) {
         console.error("Ошибка при получении данных:", error);
-        alert(':C');
+        alert(":C");
+        setLoading(false);
       }
     }
 
     fetchData();
   }, []);
 
+  const renderSkeletons = () => {
+    const skeletons = [];
+    for (let i = 0; i < 4; i++) {
+      skeletons.push(
+        <ContentLoader
+          key={i}
+          speed={2}
+          width={443}
+          height={554}
+          viewBox="0 0 443 554"
+          backgroundColor="#f3f3f3"
+          foregroundColor="white"
+        >
+          <rect x="0" y="0" rx="0" ry="0" width="443" height="554" />
+        </ContentLoader>
+      );
+    }
+    return skeletons;
+  };
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.content}>
-        {projects.map((item) => (
-          <PortfolioCard
-            key={item.id}
-            title={item.title}
-            description={item.description}
-            image={item.link}
-            url={item.url_to_project}
-          />
-        ))}
+        {loading
+          ? renderSkeletons()
+          : projects.map((item) => (
+              <PortfolioCard
+                key={item.id}
+                title={item.title}
+                description={item.description}
+                image={item.link}
+                url={item.url_to_project}
+              />
+            ))}
       </div>
       <div className={styles.contacts}>
         <Contacts />
--- react/src/pages/Services/Services.jsx
+++ react/src/pages/Services/Services.jsx
@@ -4,38 +4,65 @@ import Request from "../../components/Request/Request";
 import ServiceCard from "../../components/ServiceCard/ServiceCard";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import ContentLoader from "react-content-loader";
 
 function Services({ setDrawerOpened }) {
   const [favors, setFavors] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function fetchData() {
       try {
+        setLoading(true);
         const response = await axios.get(
           "https://codify.software/api/get-services"
         );
         setFavors(response.data);
+        setLoading(false);
       } catch (error) {
         console.error("Ошибка при получении данных:", error);
-        alert(':C');
+        alert(":C");
+        setLoading(false);
       }
     }
 
     fetchData();
   }, []);
 
+  const renderSkeletons = () => {
+    const skeletons = [];
+    for (let i = 0; i < 8; i++) {
+      skeletons.push(
+        <ContentLoader
+          key={i}
+          speed={2}
+          width={446}
+          height={226}
+          viewBox="0 0 446 226"
+          backgroundColor="#f3f3f3"
+          foregroundColor="white"
+        >
+          <rect x="0" y="0" rx="0" ry="0" width="446" height="226" />
+        </ContentLoader>
+      );
+    }
+    return skeletons;
+  };
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.content}>
-        {favors.map((item) => (
-          <ServiceCard
-            key={item.id}
-            title={item.title}
-            description={item.description}
-            price={item.price}
-            setDrawerOpened={setDrawerOpened}
-          />
-        ))}
+        {loading
+          ? renderSkeletons()
+          : favors.map((item) => (
+              <ServiceCard
+                key={item.id}
+                title={item.title}
+                description={item.description}
+                price={item.price}
+                setDrawerOpened={setDrawerOpened}
+              />
+            ))}
       </div>
       <div className={styles.contacts}>
         <Contacts />
